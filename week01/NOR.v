1943287e490eec8cb9b13e92b784473990c765ed
DBTMDDN <ohkyounghwa05@gmail.com>
2025-10-10 22:51:27 +0900

    Fix to v files

---
+++ week01/NOR.v
@@ -0,0 +1,7 @@
+//NOR
+module top_module(
+input a,
+input b,
+output out );
+assign out=~(a|b);
+endmodule
---
+++ week01/XNOR.v
@@ -0,0 +1,19 @@
+//XNOR
+module top_module(
+input a,
+input b,
+output out );
+assign out=~(a^b);
+endmodule
+Declaring wires
+`default_nettype none
+module top_module(
+input a,
+input b,
+input c,
+input d,
+output out,
+output out_n );
+assign out=(a&b)|(c&d);
+assign out_n=!((a&b)|(c&d));
+endmodule
